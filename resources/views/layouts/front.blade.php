bec694d01759953590fe5694efebb3484602eb67
Jorge <jararze@gmail.com>
2023-09-05 18:42:38 -0400

    tag google

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-R043QNGF4P"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-R043QNGF4P');
+    </script>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
@@ -35,7 +44,7 @@
 <body>
 @if(Request::is('/'))
 @endif
-{{--@include('layouts.frontend.loader')--}}
+@include('layouts.frontend.loader')
 
 @include('layouts.frontend.header')
 
